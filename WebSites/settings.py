98be75d44c9e0cc299f0bb4183af68d7d44adc51
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-05-18 16:51:17 +0530

    Update: Nothing new
    usings this to make two different versions of my website for scotopia
    
    this is web version alpha (last update on 18/5/23) and i'm planning to change the design but
    
    i don't want to delete it either so I'm gonna leave playgo1 as version not to be merged into main

--- WebSites/settings.py
+++ WebSites/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-yv4jex+#^pj!gwf(00g*f4300ts#7h5%3z*fn)7fz51$fmwku(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.20.10.7']
 
 
 # Application definition
