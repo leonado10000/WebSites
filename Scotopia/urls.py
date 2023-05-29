b92e0d19091d20f06fe140ffcc8d2deef300e013
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-05-29 13:11:08 +0530

    final design 1
    on top of general changes there's also new design for scotopia main page and its contents
    and a new feature of adding a photo to each post has also been added , and like system is also virtualized ,I can go and create a like To any post to my desire
    
    PerfectO

--- Scotopia/urls.py
+++ Scotopia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('/scoto', views.scoto , name="scoto"),
-    path('/star/<int:page_id>', views.idea, name="idea")
+    path('/add', views.Add, name="add")
     # path('/addIdea'),
 ]
+#   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
